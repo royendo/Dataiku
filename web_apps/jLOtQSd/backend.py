7342a32997f18ee77d6a639ae858dbf020e3901b
admin <admin>
2021-06-24 08:40:34 +0900

    Saved web app 23938 (EVERYTHING.jLOtQSd)

--- web_apps/jLOtQSd/backend.py
+++ web_apps/jLOtQSd/backend.py
@@ -1,22 +1,18 @@
 import dataiku
-import pandas as pd
+#import pandas as pd
 from flask import request
+import requests
+import re
+from urllib.parse import urlparse, parse_qsl, urlencode, urlunparse
 
 
-# Example:
-# As the Python webapp backend is a Flask app, refer to the Flask
-# documentation for more information about how to adapt this
-# example to your needs.
-# From JavaScript, you can access the defined endpoints using
-# getWebAppBackendUrl('first_api_call')
+@app.route('/test')
+def test():
+    print ("TEST")
+    return "TEST"
 
-@app.route('/first_api_call')
-def first_call():
-    max_rows = request.args.get('max_rows') if 'max_rows' in request.args else 500
-
-    mydataset = dataiku.Dataset("REPLACE_WITH_YOUR_DATASET_NAME")
-    mydataset_df = mydataset.get_dataframe(sampling='head', limit=max_rows)
-
-    # Pandas dataFrames are not directly JSON serializable, use to_json()
-    data = mydataset_df.to_json()
-    return json.dumps({"status": "ok", "data": data})
+@app.route('/<path:url>')
+def reverse_proxy(url):
+    print ("before ",url)
+       
+    return url
