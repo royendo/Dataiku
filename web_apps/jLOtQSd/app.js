48f48e61bb553bdc3dbc91f6e78814fd4893090f
admin <admin>
2021-06-24 08:51:14 +0900

    Saved web app 23938 (EVERYTHING.jLOtQSd)

--- web_apps/jLOtQSd/app.js
+++ web_apps/jLOtQSd/app.js
@@ -1,11 +1,1 @@
 $.get(getWebAppBackendUrl('/test'))
-$.get(getWebAppBackendUrl('/cluster/apps/RUNNING'), function(data) {
-    console.log(data)
-    $("#regTitle").html(data)
-})
-
-$.getJSON(getWebAppBackendUrl('/first_api_call'), function(data) {
-    console.log('Received data from backend', data)
-    const output = $('<pre />').text('Backend reply: ' + JSON.stringify(data));
-    $('body').append(output)
-});
