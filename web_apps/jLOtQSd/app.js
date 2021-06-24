f61b1ce91a71d10ba6f440234d6b359c0065ab94
admin <admin>
2021-06-24 14:00:16 +0900

    Saved web app 23938 (EVERYTHING.jLOtQSd)

--- web_apps/jLOtQSd/app.js
+++ web_apps/jLOtQSd/app.js
@@ -1,4 +1,8 @@
-$.get(getWebAppBackendUrl('/test'))
+$.get(getWebAppBackendUrl('/cluster/apps/RUNNING'), function(data) {
+    console.log(data)
+    $("#regTitle").html(data)
+})
+
 $.get(getWebAppBackendUrl('/cluster/apps/RUNNING'), function(data) {
     console.log(data)
     $("#regTitle").html(data)
