f46b82f93dc816eb423d0c7bbb0b72242050c7f4
admin <admin>
2021-06-30 09:27:12 +0900

    Saved recipe: EVERYTHING.compute_b

---
+++ recipes/compute_b.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+toOracle_copy = dataiku.Dataset("ToOracle_copy")
+toOracle_copy_df = toOracle_copy.get_dataframe()
+
+
+# Compute recipe outputs from inputs
+# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
+# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+
+b_df = toOracle_copy_df # For this sample code, simply copy input to output
+
+
+# Write recipe outputs
+b = dataiku.Dataset("b")
+b.write_with_schema(b_df)
