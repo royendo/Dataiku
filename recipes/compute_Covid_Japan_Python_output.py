ea9eeab45f307092dff26c770c6fa2b98e90a499
admin <admin>
2021-07-06 16:20:45 +0900

    Recipe EVERYTHING.compute_Covid_Japan_Python_output saved through notebook notebook editor for compute_Covid_Japan_Python_output

--- recipes/compute_Covid_Japan_Python_output.py
+++ recipes/compute_Covid_Japan_Python_output.py
@@ -11,7 +11,11 @@ Corona_prepared_ColumnLength = dataiku.Dataset("Corona_prepared_ColumnLength")
 
 covid_jpn_total_prepared_windows_prepared_df = covid_jpn_total_prepared_windows_prepared.get_dataframe()
 df = Corona_prepared_ColumnLength.get_dataframe()
+covid_jpn_total_prepared_windows_prepared
 covid_jpn_total_prepared_windows_prepared.write_with_schema(df)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+covid_jpn_total_prepared_windows_prepared
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
@@ -24,7 +28,7 @@ covid_jpn_total_prepared_windows_prepared_df['Hosp_mild'] = covid_jpn_total_prep
 covid_jpn_total_prepared_windows_prepared_df['Hosp_unknown'] = covid_jpn_total_prepared_windows_prepared_df['Hosp_unknown'].fillna(value)
 covid_jpn_total_prepared_windows_prepared_df['Hosp_waiting'] = covid_jpn_total_prepared_windows_prepared_df['Hosp_waiting'].fillna(value)
 
-# arithmetic doesnt work if the value in a dataset is null? 
+# arithmetic doesnt work if the value in a dataset is null?
 
 #covid_Japan_Python_output_df = covid_jpn_total_prepared_windows_prepared_df # For this sample code, simply copy input to output
 CJP_output = covid_jpn_total_prepared_windows_prepared_df.assign(hospital_total=covid_jpn_total_prepared_windows_prepared_df.Hosp_require+
@@ -33,7 +37,7 @@ CJP_output = covid_jpn_total_prepared_windows_prepared_df.assign(hospital_total=
                                                                  covid_jpn_total_prepared_windows_prepared_df.Hosp_unknown+
                                                                  covid_jpn_total_prepared_windows_prepared_df.Hosp_waiting,
                                                                 Positive1 = covid_jpn_total_prepared_windows_prepared_df.Positive - covid_jpn_total_prepared_windows_prepared_df.Asymptomatic
-                                                                
+
                                                                 ).groupby(by="Location"
                                                                          ).agg({"hospital_total":"sum",
                                                                                "Positive1":"sum"})
