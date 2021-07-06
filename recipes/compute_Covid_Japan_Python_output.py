3f04a96311e386a37444a4a26e384e8f173d8ce6
admin <admin>
2021-07-06 15:37:17 +0900

    Recipe EVERYTHING.compute_Covid_Japan_Python_output saved through notebook notebook editor for compute_Covid_Japan_Python_output

--- recipes/compute_Covid_Japan_Python_output.py
+++ recipes/compute_Covid_Japan_Python_output.py
@@ -1,20 +1,22 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 covid_jpn_total_prepared_windows_prepared = dataiku.Dataset("covid_jpn_total_prepared_windows_prepared")
 covid_jpn_total_prepared_windows_prepared_df = covid_jpn_total_prepared_windows_prepared.get_dataframe()
 
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
-covid_Japan_Python_output_df = covid_jpn_total_prepared_windows_prepared_df # For this sample code, simply copy input to output
+#covid_Japan_Python_output_df = covid_jpn_total_prepared_windows_prepared_df # For this sample code, simply copy input to output
 
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 covid_Japan_Python_output = dataiku.Dataset("Covid_Japan_Python_output")
 covid_Japan_Python_output.write_with_schema(covid_Japan_Python_output_df)
