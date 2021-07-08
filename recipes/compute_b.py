9a1f99b02bbba28a6dfdf70bd295c99383cc5b6e
admin <admin>
2021-07-09 08:28:27 +0900

    Recipe EVERYTHING.compute_b saved through notebook notebook editor for compute_b

--- recipes/compute_b.py
+++ recipes/compute_b.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -6,8 +7,10 @@ from dataiku import pandasutils as pdu
 # Read recipe inputs
 toOracle_copy = dataiku.Dataset("ToOracle_copy")
 toOracle_copy_df = toOracle_copy.get_dataframe()
-towritefolder = dataiku.Folder("O5vRK79")
-
+towritefolder = dataiku.Folder("test")
+paths = towritefolder.list_paths_in_partition()
+with towritefolder.get_writer("myoutputfile.txt") as w:
+    w.write("some data")
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
