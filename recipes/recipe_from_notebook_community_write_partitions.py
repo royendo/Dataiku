4fa91cb398b66da78fa5543e5d3cce492b6a667b
admin <admin>
2021-07-09 09:59:12 +0900

    Recipe EVERYTHING.recipe_from_notebook_community_write_partitions saved through notebook notebook editor for recipe_from_notebook_community_write_partitions

--- recipes/recipe_from_notebook_community_write_partitions.py
+++ recipes/recipe_from_notebook_community_write_partitions.py
@@ -12,19 +12,17 @@ import pandas as pd
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Example: load a DSS dataset as a Pandas dataframe
 mydataset = dataiku.Dataset("Corona1_partitioned")
-myoutputdataset = dataiku.Dataset("New_partitioned")
 mydataset_df = mydataset.get_dataframe()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-with myoutputdataset.get_writer() as writer:
-    for p in mydataset.list_partitions():
-        #print(p)
-        mydataset.read_partitions = [p]
-        df = mydataset.get_dataframe()
-        print(p, df.shape)
-        writer.write_dataframe(df)
+myoutputdataset = dataiku.Dataset("New_partitioned")
+#myoutputdataset_df = myoutputdataset.get_dataframe()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Recipe outputs
-new_partitioned = dataiku.Dataset("New_partitioned")
-new_partitioned.write_with_schema(pandas_dataframe)
+#with myoutputdataset.get_writer() as writer:
+for p in mydataset.list_partitions():
+    #print(p)
+    mydataset.read_partitions = [p]
+    df = mydataset.get_dataframe()
+    print(p, df.shape)
+    myoutputdataset.write_dataframe(df, True, True)
