d33d694f7f13c111ce82b8c48bfcc6287cdf053e
admin <admin>
2021-07-27 14:17:02 +0900

    Saved recipe: EVERYTHING.compute_pCkKYlgx

---
+++ recipes/compute_pCkKYlgx.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+fitness = dataiku.Dataset("FITNESS")
+fitness_df = fitness.get_dataframe()
+
+
+
+
+# Write recipe outputs
+s3 = dataiku.Folder("pCkKYlgx")
+s3_info = s3.get_info()
