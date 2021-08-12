f1b8a1f74c4e6ebb87da87634e5dda30ea05e6c8
admin <admin>
2021-08-12 17:54:08 +0900

    Saved recipe: EVERYTHING.compute_GCS

---
+++ recipes/compute_GCS.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+with tempfile.TemporaryDirectory() as temp_dir_name:
+   # selected_partition_year = dataiku.dku_flow_variables["DKU_DST_year"]
+
+    # Read recipe inputs
+    cds = dataiku.Folder("era5-netcfd")
+    paths = cds.list_paths_in_partition()
+    #folder = cds.get_partition_folder(selected_partition_year)
+
+   # copy_file_to_temp_folder(cds, os.path.join(folder, "era5.nc"), temp_dir_name)
+
+    #for path in paths:
+    #with cds.get_download_stream(os.path.join(folder, "era5.nc")) as f:
+    #dataset = Dataset("temp.nc", memory=f.read(), diskless=True)
+    #print("dataset file format", dataset.file_format)
+    print("dir", str(temp_dir_name+os.path.join(folder, "era5.nc")))
+    ds = xr.load_dataset(str(temp_dir_name+os.path.join(folder, "era5.nc")), chunks='auto')
+    cds_parsed_df = ds.to_dataframe()
+    cds_parsed_df.reset_index(inplace=True)  
+
+
+    # Write recipe outputs
+    cds_parsed = dataiku.Dataset("cds_parsed")
+    cds_parsed.write_with_schema(cds_parsed_df)
