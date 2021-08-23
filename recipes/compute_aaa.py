b91e49ffbf37d750874f49a839b5c2a3d78c08ce
admin <admin>
2021-08-23 13:31:48 -0400

    Saved recipe: EVERYTHING.compute_aaa

---
+++ recipes/compute_aaa.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+filesystem = dataiku.Folder("qWeVti46")
+filesystem_info = filesystem.get_info()
+
+
+# Compute recipe outputs
+# TODO: Write here your actual code that computes the outputs
+# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
+
+aaa_df = ... # Compute a Pandas dataframe to write into aaa
+
+
+# Write recipe outputs
+aaa = dataiku.Dataset("aaa")
+aaa.write_with_schema(aaa_df)
