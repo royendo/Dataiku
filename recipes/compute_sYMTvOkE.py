62fa53d290c6497226976b99e892fafa9c5fcee2
admin <admin>
2021-09-01 14:02:27 -0400

    Saved recipe: EVERYTHING.compute_sYMTvOkE

---
+++ recipes/compute_sYMTvOkE.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+managed_folder = dataiku.Folder("375D1F5f")
+managed_folder_info = managed_folder.get_info()
+
+managed
+
+
+
+# Write recipe outputs
+sdfsdf = dataiku.Folder("sYMTvOkE")
+sdfsdf_info = sdfsdf.get_info()
