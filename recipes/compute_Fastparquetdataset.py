4a71741d68bd732ea1d4168b82cdbf53ca930130
admin <admin>
2021-09-02 11:58:53 -0400

    Saved recipe: EVERYTHING.compute_Fastparquetdataset

--- recipes/compute_Fastparquetdataset.py
+++ recipes/compute_Fastparquetdataset.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
+import time
 from dataiku import pandasutils as pdu
 
 # Read recipe inputs
 fitness_prepared = dataiku.Dataset("FITNESS_prepared")
 fitness_prepared_df = fitness_prepared.get_dataframe()
 
-
+time.sleep(3000)
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
