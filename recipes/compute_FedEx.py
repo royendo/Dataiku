fd4a4f9060331b63c052a2a038446ad2814b0c80
admin <admin>
2021-09-03 15:28:23 -0400

    Saved recipe: EVERYTHING.compute_FedEx

--- recipes/compute_FedEx.py
+++ recipes/compute_FedEx.py
@@ -2,7 +2,7 @@
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
-
+'''
 # Read recipe inputs
 coronsES = dataiku.Dataset("CoronsES")
 coronsES_df = coronsES.get_dataframe()
@@ -18,3 +18,16 @@ fedEx_df = coronsES_df # For this sample code, simply copy input to output
 # Write recipe outputs
 fedEx = dataiku.Dataset("FedEx")
 fedEx.write_with_schema(fedEx_df)
+
+'''
+
+client = dataiku.api_client()
+project = client.get_project("EVERYTHING")
+
+# get unique value pairs on which to filter dataframe records:
+dsoutname = "iprobablywillforget"
+project.create_dataset(dsoutname, 'Filesystem')
+# write filtered dataframe to new dataset:
+dataset_out = dataiku.Dataset(dsoutname)
+with dataset_out.get_writer() as writer:
+    writer.write_dataframe(dataframe_out)
