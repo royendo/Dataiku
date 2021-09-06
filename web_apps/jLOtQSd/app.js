23add3d624a79e52ac0c8518be4bc2dd56af8276
admin <admin>
2021-09-06 11:49:14 -0400

    Deleted web app EVERYTHING.jLOtQSd

--- web_apps/jLOtQSd/app.js
+++
@@ -1,9 +0,0 @@
-$.get(getWebAppBackendUrl('/test'), function(data) {
-    console.log(data)
-})
-
-$.get(getWebAppBackendUrl('/cluster/apps/RUNNING'), function(data) {
-    console.log(data)
-    $("#regTitle").html(data)
-})
-
