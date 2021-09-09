b7ce200f5fa3c28a79199ef7de4e03b3468c5c81
admin <admin>
2021-09-09 16:54:35 -0400

    Saved recipe: EVERYTHING.compute_aaa

--- recipes/compute_aaa.py
+++ recipes/compute_aaa.py
@@ -12,11 +12,3 @@ time.sleep(3000)
 
 # Compute recipe outputs
 # TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
-
-aaa_df = ... # Compute a Pandas dataframe to write into aaa
-
-
-# Write recipe outputs
-aaa = dataiku.Dataset("aaa")
-aaa.write_with_schema(aaa_df)
