14741866af612fb12c9ccd278cf40fb0eee8b827
admin <admin>
2021-09-09 16:54:21 -0400

    Saved recipe: EVERYTHING.compute_aaa

--- recipes/compute_aaa.py
+++ recipes/compute_aaa.py
@@ -2,11 +2,13 @@
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+import time
 
 # Read recipe inputs
 filesystem = dataiku.Folder("qWeVti46")
 filesystem_info = filesystem.get_info()
 
+time.sleep(3000)
 
 # Compute recipe outputs
 # TODO: Write here your actual code that computes the outputs
