036c4bf290d1d5391108ff0cca9a24a27f84fa1b
admin <admin>
2021-09-22 09:29:56 -0400

    Recipe EVERYTHING.compute_communitypython saved through notebook notebook editor for compute_communitypython

--- recipes/compute_communitypython.py
+++ recipes/compute_communitypython.py
@@ -1,13 +1,31 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 # Read recipe inputs
-community = dataiku.Dataset("community")
+community = dataiku.Dataset("community_prepared")
 community_df = community.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+unique_id = []
+for ind in community_df.index:
+    if community_df['id'][ind] not in unique_id:
+        unique_id.append(community_df['id'][ind])
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+print(unique_id)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+for val in unique_id:
+    
+    for ind in community_df.index:
+        if community_df['id'][ind] == val:
+            x=x-int(community_df['value'][ind])
+    print(x)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
