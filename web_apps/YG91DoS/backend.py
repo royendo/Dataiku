0d4ff77c9afc0c2ece38d587dc46d960060464cc
admin <admin>
2021-09-30 14:16:20 -0400

    Saved web app a (EVERYTHING.YG91DoS)

--- web_apps/YG91DoS/backend.py
+++ web_apps/YG91DoS/backend.py
@@ -10,13 +10,3 @@ from flask import request
 # From JavaScript, you can access the defined endpoints using
 # getWebAppBackendUrl('first_api_call')
 
-@app.route('/first_api_call')
-def first_call():
-    max_rows = request.args.get('max_rows') if 'max_rows' in request.args else 500
-
-    mydataset = dataiku.Dataset("REPLACE_WITH_YOUR_DATASET_NAME")
-    mydataset_df = mydataset.get_dataframe(sampling='head', limit=max_rows)
-
-    # Pandas dataFrames are not directly JSON serializable, use to_json()
-    data = mydataset_df.to_json()
-    return json.dumps({"status": "ok", "data": data})
