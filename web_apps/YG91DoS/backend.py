15f6ade6bab87c25646eb95147bfc764fb92d856
admin <admin>
2021-09-30 14:16:51 -0400

    Saved web app a (EVERYTHING.YG91DoS)

--- web_apps/YG91DoS/backend.py
+++ web_apps/YG91DoS/backend.py
@@ -2,11 +2,3 @@ import dataiku
 import pandas as pd
 
 
-
-# Example:
-# As the Python webapp backend is a Flask app, refer to the Flask
-# documentation for more information about how to adapt this
-# example to your needs.
-# From JavaScript, you can access the defined endpoints using
-# getWebAppBackendUrl('first_api_call')
-
