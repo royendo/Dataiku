49f154d4c91132592e60941b9273a4416c0be432
admin <admin>
2021-10-11 13:35:39 +0900

    Updated recipe EVERYTHING.compute_o1

--- recipes/compute_o1.py
+++ recipes/compute_o1.py
@@ -11,16 +11,3 @@ imports3_df = imports3.get_dataframe()
 # Compute recipe outputs
 # TODO: Write here your actual code that computes the outputs
 # NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
-
-o1_df = ... # Compute a Pandas dataframe to write into o1
-o2_df = ... # Compute a Pandas dataframe to write into o2
-o3_df = ... # Compute a Pandas dataframe to write into o3
-
-
-# Write recipe outputs
-o1 = dataiku.Dataset("o1")
-o1.write_with_schema(o1_df)
-o2 = dataiku.Dataset("o2")
-o2.write_with_schema(o2_df)
-o3 = dataiku.Dataset("o3")
-o3.write_with_schema(o3_df)
