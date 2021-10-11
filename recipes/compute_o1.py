185824b36359db8bc6a429e9a12fc704b5d09092
admin <admin>
2021-10-11 14:46:25 +0900

    Updated recipe EVERYTHING.compute_o1

--- recipes/compute_o1.py
+++ recipes/compute_o1.py
@@ -10,7 +10,7 @@ client = dataikuapi.DSSClient(host, apiKey)
 project = client.get_project("EVERYTHING")
 Oracle = project.get_dataset('ToOracle')
 
-connection = dataikuapi.dss.admin.DSSConnectionInfo('')
+connection = dataikuapi.dss.admin.DSSConnection('')
 
 # Compute recipe outputs
 # TODO: Write here your actual code that computes the outputs
