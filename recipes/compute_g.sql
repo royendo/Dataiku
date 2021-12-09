d6d269de83cbb805b13c355ab18516eb3e8795d8
admin <admin>
2021-12-09 16:11:40 +0900

    Saved recipe: EVERYTHING.compute_g

--- recipes/compute_g.sql
+++ recipes/compute_g.sql
@@ -1,2 +1,10 @@
-SELECT *
-  FROM "EVERYTHING_single_copy_copy"
+SELECT
+  CASE
+    WHEN
+      "EVERYTHING_single_copy_copy".flag = '0'
+        THEN
+          '1'
+  END AS flag,
+  "EVERYTHING_single_copy_copy".*
+FROM
+  "EVERYTHING_single_copy_copy"
