c2059b8e985add95f8989965f66e54932f879bec
admin <test>
2021-12-23 16:29:21 +0900

    Saved recipe: EVERYTHING.compute_testpf

--- recipes/compute_testpf.sql
+++ recipes/compute_testpf.sql
@@ -1,2 +1,9 @@
-SELECT *
-  FROM "EVERYTHING_corona_postgresql"
+create or replace function public.valid_date(p_str varchar(50))
+returns boolean as $$
+begin
+    perform p_str::date;
+    return true;
+    exception when others then
+    return false;
+end;
+$$ language plpgsql;
