76349a7ae0db5e117919f6fd50bdaa91b90a7793
admin <test>
2022-01-19 09:04:36 +0900

    Saved recipe: EVERYTHING.compute_corona_partition_test

--- recipes/compute_corona_partition_test.sql
+++ recipes/compute_corona_partition_test.sql
@@ -1,7 +1,7 @@
 insert into
     "PUBLIC"."EVERYTHING_SNOWFLAKE_CORONA_COPY"(
         SELECT
-            *
+            "iso_code", "date_parsed_formatted"
         FROM
             "PUBLIC"."EVERYTHING_SNOWFLAKE_CORONA"
         GROUP BY
