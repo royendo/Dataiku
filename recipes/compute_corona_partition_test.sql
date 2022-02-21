2ce08909b0a5b38db05ea82d0abf17f7505a23e7
admin <test>
2022-02-21 15:10:27 +0900

    Saved recipe: EVERYTHING.compute_corona_partition_test

--- recipes/compute_corona_partition_test.sql
+++ recipes/compute_corona_partition_test.sql
@@ -1,9 +1,9 @@
 insert into
-    "PUBLIC"."EVERYTHING_SNOWFLAKE_CORONA_COPY"(
+    "PUBLIC"."${projectKey}_SNOWFLAKE_CORONA_COPY"(
         SELECT
             *
         FROM
-            "PUBLIC"."EVERYTHING_SNOWFLAKE_CORONA"
+            "PUBLIC"."${projectKey}_SNOWFLAKE_CORONA"
     );
 
 
