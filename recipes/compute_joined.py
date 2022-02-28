5215253ecbac608d59c90cd2c7075e5857a7ac52
admin <test>
2022-02-28 11:53:14 +0900

    Saved recipe: EVERYTHING.compute_joined

--- recipes/compute_joined.py
+++ recipes/compute_joined.py
@@ -7,26 +7,10 @@ from dataiku import pandasutils as pdu
 # Read recipe inputs
 dataset_2 = dataiku.Dataset("dataset_2")
 dataset_2_df = dataset_2.get_dataframe()
-dataset_2_df.dataset_2_col.astype(object)
 
 dataset_1 = dataiku.Dataset("dataset_1")
 dataset_1_df = dataset_1.get_dataframe()
-dataset_1_df.joining.astype(str)
 
-
-
-
-# Compute recipe outputs
-# TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-dataset_2_df
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-dataset_1_df.dtypes
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 
 joined_df = dataset_2_df.join(dataset_1_df)[source]
 
