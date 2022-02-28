90d01510fb118422d41502cf48293a96e2d5e371
admin <test>
2022-03-01 08:35:59 +0900

    Saved web app 26310 (EVERYTHING.ZoSEHRl)

--- web_apps/ZoSEHRl/backend.py
+++ web_apps/ZoSEHRl/backend.py
@@ -1,59 +1,9 @@
-import dash
-import dash_bootstrap_components as dbc
-from dash import Input, Output, dcc, html
-app.config.external_stylesheets = [dbc.themes.BOOTSTRAP]
-# the style arguments for the sidebar. We use position:fixed and a fixed width
-SIDEBAR_STYLE = {
-    "position": "fixed",
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
-    "width": "16rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
-}
-# the styles for the main content position it to the right of the sidebar and
-# add some padding.
-CONTENT_STYLE = {
-    "margin-left": "18rem",
-    "margin-right": "2rem",
-    "padding": "2rem 1rem",
-}
-sidebar = html.Div(
-    [
-        html.H2("Sidebar", className="display-4"),
-        html.Hr(),
-        html.P(
-            "A simple sidebar layout with navigation links", className="lead"
-        ),
-        dbc.Nav(
-            [
-                dbc.NavLink("Home", href="/", active="exact"),
-                dbc.NavLink("Page 1", href="/page-1", active="exact"),
-                dbc.NavLink("Page 2", href="/page-2", active="exact"),
-            ],
-            vertical=True,
-            pills=True,
-        ),
-    ],
-    style=SIDEBAR_STYLE,
+from flask import request
+@app.callback(
+    Output(component_id='my-output', component_property='children'),
+    [Input(component_id='my-input', component_property='value')]
 )
-content = html.Div(id="page-content", style=CONTENT_STYLE)
-app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
-@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
-
-def render_page_content(pathname):
-    if pathname == pathname:
-        return html.P(pathname)
-    elif pathname == "/page-1":
-        return html.P("This is the content of page 1. Yay!")
-    elif pathname == "/page-2":
-        return html.P("Oh cool, this is page 2!")
-    # If the user tries to reach a different page, return a 404 message
-    return dbc.Jumbotron(
-        [
-            html.H1("404: Not found", className="text-danger"),
-            html.Hr(),
-            html.P(f"The pathname {pathname} was not recognised..."),
-        ]
-    )
+def update_output_div(input_value):
+    request_headers = dict(request.headers)
+    auth_info_brower = dataiku.api_client().get_auth_info_from_browser_headers(request_headers)
+    return auth_info_brower["authIdentifier"]
