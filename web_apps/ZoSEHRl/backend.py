46be60509dfbccca2b678cbb7e221a2b1d26cc26
admin <test>
2022-03-01 08:36:57 +0900

    Saved web app 26310 (EVERYTHING.ZoSEHRl)

--- web_apps/ZoSEHRl/backend.py
+++ web_apps/ZoSEHRl/backend.py
@@ -1,8 +1,5 @@
 from flask import request
-@app.callback(
-    Output(component_id='my-output', component_property='children'),
-    [Input(component_id='my-input', component_property='value')]
-)
+@app.callback(Output(component_id='my-output', component_property='children'),[Input(component_id='my-input', component_property='value')])
 def update_output_div(input_value):
     request_headers = dict(request.headers)
     auth_info_brower = dataiku.api_client().get_auth_info_from_browser_headers(request_headers)
