9fb39d5e76809608bd92fdeb3acbda525d91c8f5
admin <admin>
2022-05-04 14:12:25 +0900

    Recipe EVERYTHING.compute_t saved through notebook notebook editor for compute_t

--- recipes/compute_t.py
+++ recipes/compute_t.py
@@ -1,8 +1,9 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 # Read recipe inputs
-26698_purchase_amount_ranked_1 = dataiku.Dataset("26698_purchase_amount_ranked_1")
-26698_purchase_amount_ranked_1_df = 26698_purchase_amount_ranked_1.get_dataframe()
+a = dataiku.Dataset("26698_purchase_amount_ranked_1")
+adf = a.get_dataframe()
