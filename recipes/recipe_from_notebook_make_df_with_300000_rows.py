ba74622132563f58bc298bac5e8ed7872a4ab428
admin <admin>
2022-07-05 11:52:57 +0900

    Created python recipe: EVERYTHING.recipe_from_notebook_make_df_with_300000_rows

---
+++ recipes/recipe_from_notebook_make_df_with_300000_rows.py
@@ -0,0 +1,25 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+import pandas as pd
+import dataiku
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+row = []
+for i in range(300000):
+    row.append(['x', i])
+print(row)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df = pd.DataFrame(row, columns=["X", "Value"])
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+
+py_recipe_output = dataiku.Dataset("dataset")
+py_recipe_output.write_with_schema(df)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Recipe outputs
+dataset = dataiku.Dataset("dataset")
+dataset.write_with_schema(pandas_dataframe)
