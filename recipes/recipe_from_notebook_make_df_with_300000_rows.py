c04ec1f44b996f63be1ee6231aaf4759e5b4c9a4
admin <admin>
2022-07-05 14:28:35 +0900

    Saved recipe: EVERYTHING.recipe_from_notebook_make_df_with_300000_rows

--- recipes/recipe_from_notebook_make_df_with_300000_rows.py
+++ recipes/recipe_from_notebook_make_df_with_300000_rows.py
@@ -1,20 +1,27 @@
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-import pandas as pd
+# -*- coding: utf-8 -*-
 import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-row = []
-for i in range(300000):
-    row.append(['x', i])
-print(row)
+np.random.seed(0)
+
+order_date = pd.date_range('2014-01-01 00:00:00', '2021-12-31 23:59:59', freq='min')  # For 8 years
+exec_date  = pd.date_range('2014-01-02 00:00:00', '2022-01-01 23:59:59', freq='min') 
+num_row = len(order_date) #  4.2M
+hold_time = [np.random.randint(1, 1540000) for _ in range(num_row)]  # Random Int 
+user_id = [np.random.randint(10000, 10020) for _ in range(num_row)]
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-df = pd.DataFrame(row, columns=["X", "Value"])
+data = {'Order_DateTime':order_date,
+        'Exec_DateTime':exec_date,
+        'Hold_Time':hold_time,
+        'User_Id':user_id}
+
+df = pd.DataFrame(data)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-df
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-
-py_recipe_output = dataiku.Dataset("dataset")
-py_recipe_output.write_with_schema(df)
+# Write recipe outputs
+output_ds = dataiku.Dataset("work3")
+output_ds.write_with_schema(df)
