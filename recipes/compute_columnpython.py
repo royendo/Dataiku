74913ef1b19916dea3369b19b4d3161d88bb4fe6
admin <admin>
2022-07-11 11:31:26 +0900

    Saved recipe: EVERYTHING.compute_columnpython

--- recipes/compute_columnpython.py
+++ recipes/compute_columnpython.py
@@ -6,19 +6,16 @@ from dataiku import pandasutils as pdu
 
 # Read recipe inputs
 testcolumn = dataiku.Dataset("testcolumn")
-schema = testcolumn.read_schema()
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-schema[0]['type'] = 'int'
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 testcolumn_df = testcolumn.get_dataframe()
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+
+schema = testcolumn.read_schema()
+schema[0]['type'] = 'int'
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
-
 columnpython_df = testcolumn_df # For this sample code, simply copy input to output
 
 
