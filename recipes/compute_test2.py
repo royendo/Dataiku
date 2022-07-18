7eb1a6d89eb25bda68b99927b5614ab6ee110b70
admin <admin>
2022-07-18 10:31:58 +0900

    Saved recipe: EVERYTHING.compute_test2

--- recipes/compute_test2.py
+++ recipes/compute_test2.py
@@ -4,15 +4,15 @@ import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 # Read recipe inputs
-26698_purchase_amount_ranked_1_2 = dataiku.Dataset("26698_purchase_amount_ranked_1_2")
-26698_purchase_amount_ranked_1_2_df = 26698_purchase_amount_ranked_1_2.get_dataframe()
+purchase_amount_ranked_1_2 = dataiku.Dataset("26698_purchase_amount_ranked_1_2")
+purchase_amount_ranked_1_2_df = purchase_amount_ranked_1_2.get_dataframe()
 
 
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
-test2_df = 26698_purchase_amount_ranked_1_2_df # For this sample code, simply copy input to output
+test2_df = purchase_amount_ranked_1_2_df # For this sample code, simply copy input to output
 
 
 # Write recipe outputs
