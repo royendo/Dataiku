282fc5d80c9386af6c980be69cb8bc18fc7963fa
admin <admin>
2022-09-14 10:55:09 +0900

    Saved web app rebuild_test (EVERYTHING.yc3Oq1q)

--- web_apps/yc3Oq1q/backend.py
+++ web_apps/yc3Oq1q/backend.py
@@ -1,5 +1,13 @@
 from bokeh.io import curdoc
+from bokeh.plotting import figure
+
 #Import data
 import dataiku
 dataset = dataiku.Dataset("xlsx")
 df = dataset.get_dataframe()
+
+x = df['S']
+y = df['SAMI'
+      
+p = figure()
+p.line(x=x, y=y)]
