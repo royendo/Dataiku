b56bb9640b934042e212e6cb595e4b2a92fc567a
admin <admin>
2022-09-14 11:01:14 +0900

    Saved web app rebuild_test (EVERYTHING.yc3Oq1q)

--- web_apps/yc3Oq1q/backend.py
+++ web_apps/yc3Oq1q/backend.py
@@ -1,17 +1,73 @@
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import ColumnDataSource
+from bokeh.layouts import row, column, gridplot
+
+import pandas as pd
+import numpy as np
+import math
+
 from bokeh.io import curdoc
-from bokeh.plotting import figure, show, output_file
 
-#Import data
-import dataiku
-dataset = dataiku.Dataset("xlsx")
-df = dataset.get_dataframe()
+x = np.arange(0, math.pi*2, 0.05)
 
-x = df['S']
-y = df['SAMI']
-print(x)
-print(y)
-p = figure(width=300, height=300)
-p.line(x=x, y=y)
-output_file("foo.html")
+df = pd.DataFrame(dict(
+    x = x,
+    sin = np.sin(x),
+    cos = np.cos(x),
+    tan = np.tan(x)
+))
 
-show(p)
+source = ColumnDataSource(data=df)
+
+#-------------------------------
+#   Sine Wave
+#-------------------------------
+p1 = figure(title = "Sine wave",
+            x_axis_label = 'x',
+            y_axis_label = 'y',
+            plot_width=300,
+            plot_height=300
+)
+p1.line('x', 'sin',
+        source=source,
+        legend_label = "sine",
+        line_width = 2)
+
+#-------------------------------
+#   Cosine Wave
+#-------------------------------
+p2 = figure(title = "Cosine wave",
+            x_axis_label = 'x',
+            y_axis_label = 'y',
+            plot_width=300,
+            plot_height=300
+)
+p2.line('x', 'cos',
+        source=source,
+        legend_label = "cos",
+        line_width = 2)
+
+#-------------------------------
+#   Tangent Wave
+#-------------------------------
+p3 = figure(title = "Tangent wave",
+            x_axis_label = 'x',
+            y_axis_label = 'y',
+            plot_width=300,
+            plot_height=300
+)
+p3.line('x', 'tan',
+        source=source,
+        legend_label = "tan",
+        line_width = 2)
+
+#-------------------------------
+#   Laying out in gridplot
+#-------------------------------
+plot = gridplot([p1, p2, p3], ncols=2, sizing_mode='stretch_both')
+
+# output_file("sine_cosine_tangent.html")
+# show(plot)
+
+curdoc().add_root(plot)
+curdoc().title = "Using the Bokeh Server"
