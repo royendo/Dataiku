3bb57cba52934addd0676dcc3aeb2d220ddb97a5
admin <admin>
2022-09-23 13:31:24 +0900

    Saved recipe: EVERYTHING.compute_oQL0pTOm

--- recipes/compute_oQL0pTOm.py
+++ recipes/compute_oQL0pTOm.py
@@ -1,5 +1,5 @@
 import dataiku
-
+'''
 client = dataiku.api_client()
 project = client.get_project('EVERYTHING')
 
@@ -15,3 +15,13 @@ folder = project.get_managed_folder("oQL0pTOm")
 # target_folder will now contain a copy of all files that were in source_folder
 future = tests.copy_to(folder)
 future.wait_for_result()
+'''
+
+
+import dataiku
+import os
+
+handle = dataiku.Folder("oQL0pTOm")
+path = handle.get_path()
+path_in = os.path.join(path, 'test.index')
+os.write_index(index,path_in) 
