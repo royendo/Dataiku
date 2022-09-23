bf6f0895c844f34d64120394143602a1e347318f
admin <admin>
2022-09-23 13:29:48 +0900

    Saved recipe: EVERYTHING.compute_oQL0pTOm

--- recipes/compute_oQL0pTOm.py
+++ recipes/compute_oQL0pTOm.py
@@ -5,12 +5,11 @@ project = client.get_project('EVERYTHING')
 
 # Read recipe inputs
 tests = project.get_managed_folder("iV6c3jWU")
-tests_info = tests.get_info()
+
 
 
 # Write recipe outputs
 folder = project.get_managed_folder("oQL0pTOm")
-folder_info = folder.get_info()
 
 
 # target_folder will now contain a copy of all files that were in source_folder
